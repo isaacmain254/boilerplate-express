7c74a331b30dd3d00dd7e6433eb0b0aaef033338
isaacmain254 <97783827+isaacmain254@users.noreply.github.com>
2023-11-15 09:34:21 +0000

    Added middleware to server static assets

--- myApp.js
+++ myApp.js
@@ -1,6 +1,8 @@
 let express = require('express');
 let app = express();
 
+// middleware for serving static files
+app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/views/index.html');
