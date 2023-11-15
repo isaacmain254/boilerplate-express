9969c69af682ac61e491eb2756613b95d69d1291
isaacmain254 <97783827+isaacmain254@users.noreply.github.com>
2023-11-15 10:44:24 +0000

    Added chain middleware to create time server

--- myApp.js
+++ myApp.js
@@ -10,6 +10,14 @@ app.use(function(req, res, next) {
     console.log(req.method + " " + req.path + " - " + req.ip);
     next();
 });
+
+// chain middleware to create a time server
+app.get('/now', function(req, res, next) {
+    req.time = new Date().toString();
+    next();
+}, function(req, res) {
+    res.send({"time": req.time});
+});
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/views/index.html');
 });
